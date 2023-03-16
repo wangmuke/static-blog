1736193fbbcc3b664611692dbe950fd714e611c1
wsw <12345678@qq.com>
2023-03-16 16:46:59 +0800

    more update

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `王慕珂's Blog`;
+export const SITE_TITLE = `Muke's Blog`;
 export const SITE_DESCRIPTION = '个人工作学习记录';
-export const SITE_EMAIL = 'no.wangmuke@qq.com'
+export const SITE_EMAIL = 'wangmuke@qq.com'
 export const SITE_NAME = '王慕珂的知识库';
 export const SITE_URL = "https://blog.wangmuke.top";
