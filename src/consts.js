58ad240eeb7d6b05b4971013302702cabf07c7e9
wangmuke <42886404+wangmuke@users.noreply.github.com>
2023-03-16 16:07:32 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `王慕珂's Blog`;
+export const SITE_DESCRIPTION = '个人工作学习记录';
+export const SITE_EMAIL = 'no.wangmuke@qq.com'
+export const SITE_NAME = '王慕珂的知识库';
+export const SITE_URL = "https://blog.wangmuke.top";
